0a6796e39fb3abcb2f7bf1f30e234edc96017401
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-02-06 19:19:12 +0200

    Add showAlert and isPalendrom

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const checkButton = document.getElementById("check-btn");
 const resultText = document.getElementById("result");
-let isPalindrome = false;
+const textInput = document.getElementById("text-input")
 
-const clearInputString = (str) => {
-    const regex = /[+-\s]/g;
-    return str.replace(regex, "");
+// const clearInputString = (str) => {
+//     const regex = /[+-\s]/g;
+//     return str.replace(regex, "");
+// }
+
+const isPalindrome = (str) => {
+    const clearedString = str.replace(/\W_]/g);
+    return clearedString === clearedString.split('').reverse().join();
+}
+
+const showAlert = (message) => {
+    alert(message);
+}
+
+const checkPalindrome = () => {
+
 }
