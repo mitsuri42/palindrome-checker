da9cb69218a8cfea473b1f08410f97724f822611
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-02-08 23:38:00 +0200

    Deleted clearinputString function

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ const checkButton = document.getElementById("check-btn");
 const resultText = document.getElementById("result");
 const textInput = document.getElementById("text-input")
 
-// const clearInputString = (str) => {
-//     const regex = /[+-\s]/g;
-//     return str.replace(regex, "");
-// }
 
 const isPalindrome = (str) => {
     const clearedString = str.toLowerCase().replace(/[\W_]/g, "");
